9b4bfe1cda928e785e71b93d015a1174df38d0b5
GlawckHs <tad-glawcksilva@ucpparana.edu.br>
2026-05-15 17:52:27 -0300

    Finalização da tela de cadastro

--- app/page.tsx
+++ app/page.tsx
@@ -19,8 +19,17 @@ export default function Cad() {
               <input type="text" placeholder="Nome Completo" required />
               <input type="email" placeholder="Email" required />
               <input type="password" placeholder="Senha" required />
+              <input type="checkbox" id="terms" required />
+              <label htmlFor="terms">Eu aceito os <a href='/terms'>Termos de Uso</a> e a <a href='/privacy'>Política de Privacidade</a></label>
               <button type="submit">Criar Conta</button>  
             </form>
+          </section>
+
+          <section>
+              <button> Continuar com Google</button>
+              <button> Continuar com Apple</button>
+
+              <p>Já tem uma conta?<a href="/login"> Entrar</a></p>
           </section>
 
         </main>
