4340577974670f12fd70e028c1360f66522f8513
GlawckHs <tad-glawcksilva@ucpparana.edu.br>
2026-05-16 11:50:29 -0300

    inserção de condições para validação de senha

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,40 @@
+'use client'
+
 import { supabase} from '@/lib/supabase'
+import React, { useState} from 'react'
 
 
 export default function Cad() {
+  const [ nome, setNome] = useState('')
+  const [ email, setEmail] = useState('')
+  const [ senha, setSenha] = useState('')
+  const [confSenha, setConfSenha] =useState('')
+  const especiais = /[!@#$%^&*(),.?":{}|<>]/.test(senha)
+
+
+  function envioInfo(e: React.FormEvent){
+    e.preventDefault()
+
+    console.log(nome) 
+    console.log(email)
+    console.log(senha)
+    console.log(confSenha)
+
+    if(senha.length < 8){
+      alert(' A senha deve conter no minimo 8 caracteres')
+    }
+
+    if(!especiais) {
+      alert(' A senha deve conter no minimo um caractere especial')
+
+    }
+
+    if(senha !==confSenha){
+      alert(' As senhas não coincidem')
+    }
+  };
+
+
 
     return (
         <main>
@@ -15,10 +48,12 @@ export default function Cad() {
           </section>
 
           <section>
-            <form>
-              <input type="text" placeholder="Nome Completo" required />
-              <input type="email" placeholder="Email" required />
-              <input type="password" placeholder="Senha" required />
+            <form onSubmit={envioInfo}>
+              <input type="text" placeholder="Nome Completo" value={nome} onChange={(e) => setNome(e.target.value)} required />
+              <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+              <input type="password" placeholder="Senha" value={senha} onChange={(e) => setSenha(e.target.value)} required />
+              <p>Confirme sua senha</p>
+              <input type="password" placeholder="Confirmação da Senha" value={confSenha} onChange={(e) => setConfSenha(e.target.value)} required />
               <input type="checkbox" id="terms" required />
               <label htmlFor="terms">Eu aceito os <a href='/terms'>Termos de Uso</a> e a <a href='/privacy'>Política de Privacidade</a></label>
               <button type="submit">Criar Conta</button>  
